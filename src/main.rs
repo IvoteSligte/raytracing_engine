1277b8aab5fc45781af963e5a296f96140b3f4ab
Ivo te Sligte <ivotesligte@gmail.com>
2022-08-11 13:43:57 +0200

    A simple ray marcher with player movement and rotation. Rotation is still buggy.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant, ops::Mul};
 
+use extend::ext;
+use glam::{Quat, Vec3, UVec2, Vec2};
 use vulkano::{
-    buffer::{BufferUsage, ImmutableBuffer},
+    buffer::{BufferUsage, DeviceLocalBuffer},
     command_buffer::{
-        AutoCommandBufferBuilder,
-        CommandBufferUsage, PrimaryAutoCommandBuffer, RenderPassBeginInfo, SubpassContents,
+        AutoCommandBufferBuilder, CommandBufferUsage, PrimaryAutoCommandBuffer,
+        RenderPassBeginInfo, SubpassContents,
     },
     descriptor_set::{DescriptorSetsCollection, PersistentDescriptorSet, WriteDescriptorSet},
     device::{
@@ -29,13 +31,16 @@ use vulkano::{
     },
     sync::{self, FenceSignalFuture, FlushError, GpuFuture},
     Version,
+    VulkanLibrary,
 };
 use vulkano_win::VkSurfaceBuild;
 use winit::{
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    dpi::PhysicalPosition,
+    event::{Event, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Fullscreen, Window, WindowBuilder},
 };
+use winit_input_helper::WinitInputHelper;
 
 mod vertex_shader {
     vulkano_shaders::shader! {
@@ -63,21 +68,27 @@ mod fragment_shader {
 #version 460
 
 layout(push_constant) uniform PushConstantData {
+    vec4 rot;
     vec3 pos;
 } pc;
 
 layout(binding = 0) uniform ViewportBuffer {
-    vec2 viewport;
-} ubo;
+    vec2 size;
+} view;
 
 layout(location = 0) out vec4 fragColor;
 
-void main() {
-    vec2 normCoord = gl_FragCoord.xy / ubo.viewport - 0.5;
-    normCoord.x *= ubo.viewport.x / ubo.viewport.y;
+vec3 rotate(vec4 q, vec3 v) {
+    vec3 temp = cross(q.xyz, v) + q.w * v;
+    return v + 2.0 * cross(q.xyz, temp);
+}
 
-    vec4 light = vec4(5.0, 0.0, 5.0, 100.0); // w = strength
-    vec4 sphere = vec4(0.0, 0.0, 5.0, 2.0); // w = radius
+void main() {
+    vec2 normCoord = gl_FragCoord.xy / view.size - 0.5;
+    normCoord.x *= view.size.x / view.size.y;
+
+    vec4 light = vec4(5.0, 5.0, 0.0, 100.0); // w = strength
+    vec4 sphere = vec4(0.0, 5.0, 0.0, 2.0); // w = radius
 
     vec3 colorMat = vec3(0.2, 0.2, 1.0);
     float specularMat = 1.0;
@@ -86,21 +97,28 @@ void main() {
 
     float ambientScene = 0.8;
 
-    vec3 fragPos = vec3(normCoord, 1.0);
-    vec3 position = pc.pos + fragPos;
-    vec3 step = normalize(fragPos) / 100.0;
+    float nearClipPlane = 1.0;
+    uint farClipPlane = 1000;
+    float stepLength = 0.01;
 
-    for (uint i = 0; i < 1000; i++) {
-        if (distance(position, sphere.xyz) <= sphere.w) {
-            vec3 normal = normalize(position - sphere.xyz);
-            vec3 lightDir = normalize(light.xyz - position); // might be light.xyz - fragPos
-            vec3 reflection = reflect(lightDir, normal);
+    vec3 fragPos = vec3(normCoord.x, 1.0, normCoord.y);
+    fragPos = rotate(pc.rot, fragPos);
+    vec3 position = pc.pos + fragPos * nearClipPlane;
+    vec3 step = normalize(fragPos) * stepLength;
 
+    while (length(position) < farClipPlane) {
+        float minDist = distance(position, sphere.xyz);
+        if (minDist <= sphere.w) {
+            vec3 lightDir = normalize(light.xyz - position);
+            
             float lightDist = distance(position, light.xyz);
-            float lightDistFallOff = 1 / (lightDist * lightDist);
-
+            float lightDistFallOff = 1.0 / (lightDist * lightDist);
+            
             float camDist = distance(position, fragPos);
-            float camDistFallOff = 1 / (camDist * camDist);
+            float camDistFallOff = 1.0 / (camDist * camDist);
+
+            vec3 normal = normalize(position - sphere.xyz);
+            vec3 reflection = reflect(lightDir, normal);
 
             vec3 ambient = ambientScene * colorMat;
             vec3 diffuse = max(dot(normal, lightDir), 0.0) * light.w * diffuseMat * colorMat * lightDistFallOff;
@@ -109,10 +127,17 @@ void main() {
             fragColor = vec4((ambient + diffuse + specular) * camDistFallOff, 1.0);
             return;
         }
-        position += step;
+        position += step * minDist; // incl treshold: max(step * minDist, minStep)
     }
     fragColor = vec4(0.0, 0.0, 0.0, 1.0);
 }"
+    }
+}
+
+#[ext]
+impl Vec2 {
+    fn to_pos(self) -> PhysicalPosition<f32> {
+        PhysicalPosition::new(self.x, self.y)
     }
 }
 
@@ -199,7 +224,7 @@ fn get_descriptor_set(
     queue: Arc<Queue>,
     viewport: Viewport,
 ) -> Arc<PersistentDescriptorSet> {
-    let (uniform_buffer, _) = ImmutableBuffer::from_data(
+    let (uniform_buffer, _) = DeviceLocalBuffer::from_data(
         viewport.dimensions,
         BufferUsage::uniform_buffer(),
         queue.clone(),
@@ -255,9 +280,31 @@ where
     Arc::new(builder.build().unwrap())
 }
 
+fn get_window_center(window: &Window) -> Vec2 {
+    let size = window.inner_size();
+    UVec2::new(size.width / 2, size.height / 2).as_vec2()
+}
+
+#[allow(dead_code)]
+mod speed {
+    pub const MOVEMENT: f32 = 2.0;
+    pub const ROTATION: f32 = 1.0;
+    pub const MOUSE: f32 = 1.0;
+}
+
+#[allow(dead_code)]
+mod rotation {
+    use glam::Vec3;
+
+    pub const UP: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+    pub const FORWARD: Vec3 = Vec3::new(0.0, 1.0, 0.0);
+    pub const RIGHT: Vec3 = Vec3::new(1.0, 0.0, 0.0);
+}
+
 fn main() {
-    let required_extensions = vulkano_win::required_extensions();
-    let instance = Instance::new(InstanceCreateInfo {
+    let library = VulkanLibrary::new().unwrap();
+    let required_extensions = vulkano_win::required_extensions(&library);
+    let instance = Instance::new(library, InstanceCreateInfo {
         enabled_extensions: required_extensions,
         engine_version: Version::V1_3,
         ..Default::default()
@@ -265,9 +312,8 @@ fn main() {
     .unwrap();
 
     let event_loop = EventLoop::new();
-    let surface = WindowBuilder::new()
-        .build_vk_surface(&event_loop, instance.clone())
-        .unwrap();
+    let surface = WindowBuilder::new().build_vk_surface(&event_loop, instance.clone()).unwrap();
+    surface.window().set_cursor_visible(false);
 
     let device_extensions = DeviceExtensions {
         khr_swapchain: true,
@@ -346,6 +392,7 @@ fn main() {
     );
 
     let mut push_constants = fragment_shader::ty::PushConstantData {
+        rot: [0.0, 0.0, 0.0, 1.0],
         pos: [0.0, 0.0, 0.0],
     };
 
@@ -357,157 +404,214 @@ fn main() {
     let mut fences: Vec<Option<Arc<FenceSignalFuture<_>>>> = vec![None; images.len()];
     let mut previous_fence_index = 0;
 
-    let mut window_minimized = false;
+    let mut window_center = get_window_center(surface.window());
+
+    let mut input_helper = WinitInputHelper::new();
+
+    #[cfg(debug_assertions)]
+    let mut fps_helper = fps_counter::FPSCounter::new();
+
+    let mut last_update = Instant::now();
+
     let mut window_resized = false;
+    let mut window_frozen = false;
     let mut recreate_swapchain = false;
 
-    surface.window().set_decorations(false);
-
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
+    // TODO: camera rolls when rotating with mouse
+    // TODO: fix rotation when resizing
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::WindowEvent {
+            event: WindowEvent::Focused(focused),
             ..
-        } => {
+        } = event
+        {
+            window_frozen = !focused;
+        }
+
+        if !input_helper.update(&event) {
+            return;
+        }
+
+        if input_helper.quit() || input_helper.key_released(VirtualKeyCode::Escape) {
             *control_flow = ControlFlow::Exit;
         }
-        Event::WindowEvent {
-            event: WindowEvent::Resized(size),
-            ..
-        } => {
-            window_minimized = size.width == 0 || size.height == 0;
+
+        if let Some(size) = input_helper.window_resized() {
+            // window minimized
+            window_frozen = size.width == 0 || size.height == 0;
             window_resized = true;
+            window_center = get_window_center(surface.window());
+            viewport.dimensions = window_center.mul(2.0).to_array();
         }
-        Event::WindowEvent {
-            event:
-                WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
-                            virtual_keycode: Some(keycode),
-                            state,
-                            ..
-                        },
-                    ..
-                },
-            ..
-        } => match (keycode, state) {
-            (VirtualKeyCode::F11, ElementState::Pressed) => {
-                let window = surface.window();
-                match window.fullscreen() {
-                    Some(_) => window.set_fullscreen(None),
-                    None => window.set_fullscreen(Some(Fullscreen::Borderless(None))),
-                }
-            }
-            (VirtualKeyCode::W, ElementState::Pressed) => {
-                push_constants.pos[2] += 1.0;
-            }
-            (VirtualKeyCode::A, ElementState::Pressed) => {
-                push_constants.pos[0] -= 1.0;
-            }
-            (VirtualKeyCode::S, ElementState::Pressed) => {
-                push_constants.pos[2] -= 1.0;
-            }
-            (VirtualKeyCode::D, ElementState::Pressed) => {
-                push_constants.pos[0] += 1.0;
-            }
-            (VirtualKeyCode::Q, ElementState::Pressed) => {
-                push_constants.pos[1] += 1.0;
-            }
-            (VirtualKeyCode::E, ElementState::Pressed) => {
-                push_constants.pos[1] -= 1.0;
-            }
-            _ => (),
-        },
-        Event::MainEventsCleared if !window_minimized => {
-            if recreate_swapchain || recreate_swapchain {
-                recreate_swapchain = false;
 
-                let dimensions = surface.window().inner_size();
-
-                let (new_swapchain, images) = match swapchain.recreate(SwapchainCreateInfo {
-                    image_extent: dimensions.into(),
-                    ..swapchain.create_info()
-                }) {
-                    Ok(ok) => ok,
-                    Err(SwapchainCreationError::ImageExtentNotSupported { .. }) => return,
-                    Err(err) => panic!("{}", err),
-                };
-                swapchain = new_swapchain;
-
-                framebuffers = get_framebuffers(&images, render_pass.clone());
-
-                if window_resized {
-                    window_resized = false;
-
-                    viewport.dimensions = dimensions.into();
-
-                    graphics_pipeline = get_graphics_pipeline(
-                        device.clone(),
-                        vertex_shader.clone(),
-                        fragment_shader.clone(),
-                        viewport.clone(),
-                        render_pass.clone(),
-                    );
-
-                    descriptor_set = get_descriptor_set(
-                        graphics_pipeline.clone(),
-                        queue.clone(),
-                        viewport.clone(),
-                    );
-                }
-            }
-
-            let (image_index, suboptimal, image_future) =
-                match swapchain::acquire_next_image(swapchain.clone(), None) {
-                    Ok(ok) => ok,
-                    Err(AcquireError::OutOfDate) => {
-                        return recreate_swapchain = true;
-                    }
-                    Err(err) => panic!("{}", err),
-                };
-            recreate_swapchain |= suboptimal;
-
-            if let Some(image_fence) = &fences[image_index] {
-                image_fence.wait(None).unwrap();
-            }
-
-            command_buffers[image_index] = Some(get_primary_command_buffer(
-                device.clone(),
-                queue.clone(),
-                graphics_pipeline.clone(),
-                framebuffers[image_index].clone(),
-                push_constants.clone(),
-                descriptor_set.clone(),
-            ));
-
-            let previous_future = match fences[previous_fence_index].clone() {
-                Some(future) => future.boxed(),
-                None => {
-                    let mut future = sync::now(device.clone());
-                    future.cleanup_finished();
-                    future.boxed()
-                }
-            };
-
-            let future = previous_future
-                .join(image_future)
-                .then_execute(queue.clone(), command_buffers[image_index].clone().unwrap())
-                .unwrap()
-                .then_swapchain_present(queue.clone(), swapchain.clone(), image_index)
-                .then_signal_fence_and_flush();
-
-            fences[image_index] = match future {
-                Ok(ok) => Some(Arc::new(ok)),
-                Err(FlushError::OutOfDate) => {
-                    recreate_swapchain = true;
-                    None
-                }
-                Err(err) => {
-                    println!("{}", err);
-                    None
-                }
-            };
-            previous_fence_index = image_index;
+        surface.window().set_cursor_position(window_center.to_pos()).unwrap();
+        
+        if window_frozen {
+            return;
         }
-        _ => (),
+
+        #[cfg(debug_assertions)]
+        println!("{}", fps_helper.tick());
+
+        if input_helper.key_pressed(VirtualKeyCode::F11) {
+            let window = surface.window();
+            match window.fullscreen() {
+                Some(_) => window.set_fullscreen(None),
+                None => window.set_fullscreen(Some(Fullscreen::Borderless(None))),
+            }
+        }
+
+        let delta_time = last_update.elapsed().as_secs_f32();
+        last_update = Instant::now();
+
+        let mut rotation = Quat::from_array(push_constants.rot);
+
+        if !window_resized {
+            let pos = input_helper
+                .mouse()
+                .map(|(x, y)| Vec2::new(x, y))
+                .unwrap_or(window_center);
+    
+            let mov = (pos - window_center) / Vec2::from_array(viewport.dimensions) * speed::ROTATION;
+            println!("mov: {}, wc: {}, pos: {}", mov, window_center, pos);
+            // yaw
+            rotation *= Quat::from_axis_angle(-rotation::UP, mov.x);
+            // pitch
+            rotation *= Quat::from_axis_angle(rotation::RIGHT, mov.y);
+        }
+
+        let rot_time = delta_time * speed::ROTATION;
+
+        if input_helper.key_held(VirtualKeyCode::Left) {
+            rotation *= Quat::from_axis_angle(rotation::UP, rot_time);
+        }
+        if input_helper.key_held(VirtualKeyCode::Right) {
+            rotation *= Quat::from_axis_angle(-rotation::UP, rot_time);
+        }
+        if input_helper.key_held(VirtualKeyCode::Down) {
+            rotation *= Quat::from_axis_angle(rotation::RIGHT, rot_time);
+        }
+        if input_helper.key_held(VirtualKeyCode::Up) {
+            rotation *= Quat::from_axis_angle(-rotation::RIGHT, rot_time);
+        }
+
+        push_constants.rot = rotation.to_array();
+
+        let mov_time = delta_time * speed::MOVEMENT;
+
+        let quaternion = Quat::from_array(push_constants.rot).normalize();
+        let forward = quaternion.mul_vec3(rotation::FORWARD);
+        let right = quaternion.mul_vec3(rotation::RIGHT);
+        let up = quaternion.mul_vec3(rotation::UP);
+
+        let mut position = Vec3::from_array(push_constants.pos);
+
+        if input_helper.key_held(VirtualKeyCode::A) {
+            position -= right * mov_time;
+        }
+        if input_helper.key_held(VirtualKeyCode::D) {
+            position += right * mov_time;
+        }
+        if input_helper.key_held(VirtualKeyCode::W) {
+            position += forward * mov_time;
+        }
+        if input_helper.key_held(VirtualKeyCode::S) {
+            position -= forward * mov_time;
+        }
+        if input_helper.key_held(VirtualKeyCode::Q) {
+            position += up * mov_time;
+        }
+        if input_helper.key_held(VirtualKeyCode::E) {
+            position -= up * mov_time;
+        }
+
+        push_constants.pos = position.to_array();
+
+        if recreate_swapchain || window_resized {
+            recreate_swapchain = false;
+
+            let dimensions = surface.window().inner_size();
+
+            let (new_swapchain, images) = match swapchain.recreate(SwapchainCreateInfo {
+                image_extent: dimensions.into(),
+                ..swapchain.create_info()
+            }) {
+                Ok(ok) => ok,
+                Err(SwapchainCreationError::ImageExtentNotSupported { .. }) => return,
+                Err(err) => panic!("{}", err),
+            };
+            swapchain = new_swapchain;
+
+            framebuffers = get_framebuffers(&images, render_pass.clone());
+
+            if window_resized {
+                window_resized = false;
+
+                viewport.dimensions = dimensions.into();
+
+                graphics_pipeline = get_graphics_pipeline(
+                    device.clone(),
+                    vertex_shader.clone(),
+                    fragment_shader.clone(),
+                    viewport.clone(),
+                    render_pass.clone(),
+                );
+
+                descriptor_set =
+                    get_descriptor_set(graphics_pipeline.clone(), queue.clone(), viewport.clone());
+            }
+        }
+
+        let (image_index, suboptimal, image_future) =
+            match swapchain::acquire_next_image(swapchain.clone(), None) {
+                Ok(ok) => ok,
+                Err(AcquireError::OutOfDate) => {
+                    return recreate_swapchain = true;
+                }
+                Err(err) => panic!("{}", err),
+            };
+        recreate_swapchain |= suboptimal;
+
+        if let Some(image_fence) = &fences[image_index] {
+            image_fence.wait(None).unwrap();
+        }
+
+        command_buffers[image_index] = Some(get_primary_command_buffer(
+            device.clone(),
+            queue.clone(),
+            graphics_pipeline.clone(),
+            framebuffers[image_index].clone(),
+            push_constants.clone(),
+            descriptor_set.clone(),
+        ));
+
+        let previous_future = match fences[previous_fence_index].clone() {
+            Some(future) => future.boxed(),
+            None => {
+                let mut future = sync::now(device.clone());
+                future.cleanup_finished();
+                future.boxed()
+            }
+        };
+
+        let future = previous_future
+            .join(image_future)
+            .then_execute(queue.clone(), command_buffers[image_index].clone().unwrap())
+            .unwrap()
+            .then_swapchain_present(queue.clone(), swapchain.clone(), image_index)
+            .then_signal_fence_and_flush();
+
+        fences[image_index] = match future {
+            Ok(ok) => Some(Arc::new(ok)),
+            Err(FlushError::OutOfDate) => {
+                recreate_swapchain = true;
+                None
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+                None
+            }
+        };
+        previous_fence_index = image_index;
     })
 }
